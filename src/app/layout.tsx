5fdd636ff0460c68f4be9368ed77511294de6c78
RyoSugimoto <1042374+RyoSugimoto@users.noreply.github.com>
2025-09-29 13:38:41 +0900

    グローバルフッターを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,17 @@
 import '@/di';
 import '@/styles/globals.css';
+import Footer from '@/app/shared/components/layout/Footer';
+import Stack from '@/components/layout/Stack';
 
 export default function RootLayout({ children }) {
   return (
     <html className="dark dark:bg-gray-800 dark:text-white" lang="ja">
-      <body>{children}</body>
+      <body>
+        <Stack>
+          {children}
+          <Footer />
+        </Stack>
+      </body>
     </html>
   );
 }
--- src/app/shared/components/layout/Footer.tsx
+++ src/app/shared/components/layout/Footer.tsx
@@ -1,9 +1,11 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <footer>
-
+      <div className="container max-w-screen-xl mx-auto px-4 py-4">
+        <p>Copyright 2025 Visual Bookmark</p>
+      </div>
     </footer>
-  )
-} 
+  );
+}
